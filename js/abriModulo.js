e570bfc819283f2e282d901fe696d2a7fc76cea9
Jhonathan Espinosa <jhonathan2@gmail.com>
2016-02-11 11:13:17 -0500

    modificación distribución en formulario de Registro

--- js/abriModulo.js
+++ js/abriModulo.js
@@ -3,9 +3,10 @@ jQuery(document).ready(function($) {
 });
 function abrirModulo()
 {
-
-  $("head").abrirModulo_cargarArchivos("../admin/head.html");
-  $("head").abrirModulo_cargarArchivos("../admin/head");
+  $("fixedHead").abrirModulo_cargarArchivos("../admin/head.html");
+  $("body > footer").abrirModulo_cargarArchivos("../admin/footer.html");
+  $("body > header").abrirModulo_cargarArchivos("../admin/header.html");
+  $("body > sidebar").abrirModulo_cargarArchivos("../admin/sidebar.html");
   
 }
 $.fn.abrirModulo_cargarArchivos = function (modulo, callback)
