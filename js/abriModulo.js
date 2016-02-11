547f5dc09631e718be791fc77480df56a8d4eac3
Jhonathan Espinosa <jhonathan2@gmail.com>
2016-02-11 10:53:47 -0500

    estructuracion basica

---
+++ js/abriModulo.js
@@ -0,0 +1,10 @@
+function abrirModulo()
+{
+  var numDirectorios = parseInt(location.href.split("/").length) - 5;
+  var res = "";
+  for (var i = 0; i < numDirectorios; i++) 
+  {
+    res += "../";
+  }
+  return res;
+}
